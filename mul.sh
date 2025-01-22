046a59733ef437cf018935af186fc28efe7579c3
rajkanani17 <98444498+rajkanani17@users.noreply.github.com>
2025-01-22 10:18:16 +0530

    Update mul.sh

--- mul.sh
+++ mul.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # Print a multiplication table
 
-x = $1
-y = $2
+x=$1
+y=$2
 
-i = 1
+i=1
 while [ $i -le $y ]
 do
     echo " $i X $x = `expr $i \* $x` "
-    i = `expr $i + 1`
+    i=`expr $i + 1`
 done
